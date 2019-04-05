9c3da3dc240e2d1451a10407c0ca1f1c2b0436c1
davidalexbrown <1364ab@gmail.com>
2019-04-04 22:07:59 -0700

    fixed issue with discord.py update

--- inmsg.py
+++ inmsg.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix)
 
-class inmsg:
+class inmsg(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
